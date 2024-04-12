b31be113622ab55a938c08cb4e1ac61494b0151c
“Mari” <mbgroff@stanford.edu>
2024-04-12 13:49:50 -0300

    added home page

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('core.urls')), # pagina inicial
     path('admin/', admin.site.urls),
     path('login/', include('accounts.urls')),
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+HOME_PATH = "home.html"
+
+def home(request):
+    return render(request, HOME_PATH)
