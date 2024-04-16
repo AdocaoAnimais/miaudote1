c70eb780467e644ebcd58dd91612f47d4b68b06c
“Mari” <mbgroff@stanford.edu>
2024-04-15 22:01:04 -0300

    add coisas no custom user e manda emails

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'bio']  # Add any other fields you want to display
+
+    # Optionally, define search fields if needed
+    search_fields = ('username', 'email')
+
+    # Optionally, define fieldsets to customize the layout in the admin panel
+    # fieldsets = (
+    #     (None, {'fields': ('username', 'password')}),
+    #     ('Personal Info', {'fields': ('first_name', 'last_name', 'email', 'bio')}),
+    #     ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    #     ('Important Dates', {'fields': ('last_login', 'date_joined')}),
+    # )
+
+# Register the CustomUserAdmin class with the CustomUser model
+admin.site.register(CustomUser, CustomUserAdmin)
