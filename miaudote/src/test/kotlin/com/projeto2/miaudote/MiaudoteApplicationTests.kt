3bfbef00384278cd1c7b8985eda279ea44c4e188
Paulina Rehbein <220602eer@gmail.com>
2024-05-15 19:29:38 -0300

    feat: docker configuração

--- miaudote/src/test/kotlin/com/projeto2/miaudote/MiaudoteApplicationTests.kt
+++ miaudote/src/test/kotlin/com/projeto2/miaudote/MiaudoteApplicationTests.kt
@@ -1,13 +1,2 @@
 package com.projeto2.miaudote
 
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
-class MiaudoteApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
-}
