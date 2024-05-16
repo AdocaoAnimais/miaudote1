be5ce94ab891d4ab797c70438ec0a66dd9173480
Paulina Rehbein <220602eer@gmail.com>
2024-05-16 12:16:12 -0300

    tentativa de login

--- miaudote/src/main/kotlin/com/projeto2/miaudote/repositories/UsuarioRepository.kt
+++ miaudote/src/main/kotlin/com/projeto2/miaudote/repositories/UsuarioRepository.kt
@@ -2,5 +2,11 @@ package com.projeto2.miaudote.repositories
 
 import com.projeto2.miaudote.entities.Usuario
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
 
-interface UsuarioRepository : JpaRepository<Usuario, Long >
+
+interface UsuarioRepository : JpaRepository<Usuario, Long > {
+    fun findByUsername(@Param("username") username: String?): Usuario?
+}
