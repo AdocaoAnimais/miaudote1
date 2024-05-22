e6a497188f33fdcb7750d97a7f907817bd68f8f1
Paulina Rehbein <220602eer@gmail.com>
2024-05-22 20:24:24 -0300

    deleta projeto python e projeto api com node

--- backend/pet/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import PET
-
-
-class PetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PET
-        fields = '__all__'
