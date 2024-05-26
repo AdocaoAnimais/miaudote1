5cf34eca89fb85987e323e5a3b3ae864ec8b0964
Paulina Rehbein <220602eer@gmail.com>
2024-05-26 17:21:35 -0300

    valida solicitacao adoção

--- miaudote/src/main/kotlin/com/projeto2/miaudote/domain/entities/Adocao.kt
+++ miaudote/src/main/kotlin/com/projeto2/miaudote/domain/entities/Adocao.kt
@@ -1,10 +1,15 @@
 package com.projeto2.miaudote.domain.entities
 
-import jakarta.persistence.Entity
-import jakarta.persistence.Table
+import jakarta.persistence.*
 
 @Entity
 @Table(name = "adocao")
 class Adocao(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "adocao_id")
+    val id: Long?,
 
+    @Column(name = "pet_id")
+    val petId: Long,
 )
--- miaudote/src/main/kotlin/com/projeto2/miaudote/infraestructure/repositories/SolicitacaoAdocaoRepository.kt
+++ miaudote/src/main/kotlin/com/projeto2/miaudote/infraestructure/repositories/SolicitacaoAdocaoRepository.kt
@@ -2,12 +2,6 @@ package com.projeto2.miaudote.infraestructure.repositories
 
 import com.projeto2.miaudote.domain.entities.SolicitacaoAdocao
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
-interface SolicitacaoAdocaoRepository : JpaRepository<SolicitacaoAdocao, Long> {
-    fun findByPetId(id: Long): SolicitacaoAdocao?
-    fun findByUsuarioResponsavelOrUsuarioAdotanteAndId(
-        usuarioResponsavelId: Long,
-        petId: Long,
-        usuarioAdotanteId: Long
-    ): SolicitacaoAdocao?
-}
+interface SolicitacaoAdocaoRepository : JpaRepository<SolicitacaoAdocao, UUID>
