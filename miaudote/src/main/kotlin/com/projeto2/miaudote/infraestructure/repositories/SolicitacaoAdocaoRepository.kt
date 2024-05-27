d3f41497e338e924061bd9718784d5905c78fa26
Paulina Rehbein <220602eer@gmail.com>
2024-05-27 20:12:59 -0300

    valida solicitação de adoção

--- miaudote/src/main/kotlin/com/projeto2/miaudote/infraestructure/repositories/SolicitacaoAdocaoRepository.kt
+++ miaudote/src/main/kotlin/com/projeto2/miaudote/infraestructure/repositories/SolicitacaoAdocaoRepository.kt
@@ -4,4 +4,6 @@ import com.projeto2.miaudote.domain.entities.SolicitacaoAdocao
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
-interface SolicitacaoAdocaoRepository : JpaRepository<SolicitacaoAdocao, UUID>
+interface SolicitacaoAdocaoRepository : JpaRepository<SolicitacaoAdocao, UUID> {
+    fun findByIdAndusuarioAdotante(id: Long, usuarioAdotante: Long): SolicitacaoAdocao?
+}
