388587630129daf54a0c74da9cbe18af4b11b02e
Paulina Rehbein <220602eer@gmail.com>
2024-05-30 19:45:00 -0300

    feat: correção de login para continuar logado ao fazer reload da tela

--- front/data/AuthenticationService.ts
+++ front/data/AuthenticationService.ts
@@ -2,25 +2,40 @@ import api, { BASE_URL } from "@/app/App";
 import { LoginResponse } from "@/domain/Login";
 
 export class AuthenticationService {
+    private chave = "tokenMiaudote"
     async loggin(username: string, senha: string) {
         const params = {
             "username": username,
             "senha": senha
         }
-
+        api.defaults.headers.common['Authorization'] = null
+        window.localStorage.removeItem(this.chave)
         await api.post(`${BASE_URL}/api/auth/login`, params).then(res => {
             let response = res.data as LoginResponse 
+            window.localStorage.setItem(this.chave, response.accessToken)
             api.defaults.headers.common['Authorization'] = `Bearer ${response.accessToken}`; 
         })
     }
 
     async logged(): Promise<boolean> { 
+        const token = window.localStorage.getItem(this.chave)
+        console.log("token", token)
+        if(token!=null){
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`; 
+        }
         return await api.get(`${BASE_URL}/api/auth/logged`)
-            .then(res => true)
-            .catch((error) => false)
+            .then(res => {
+                console.log("esta logado")
+                return true
+            })
+            .catch((error) => {
+                console.log("não esta logado")
+                return false
+            })
     }
 
     logout() { 
+        window.localStorage.removeItem(this.chave)
         api.defaults.headers.common['Authorization'] = null;
     }
 }
