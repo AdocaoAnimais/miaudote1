4a36974c7ebee39613af62c413c83b697e38c121
Paulina Rehbein <220602eer@gmail.com>
2024-06-02 12:30:31 -0300

    ajusta tela principal

--- front/app/components/NavBar.tsx
+++ front/app/components/NavBar.tsx
@@ -96,7 +96,7 @@ export default function NavBar() {
                                 </div>
                             </div>
 
-                            {!logado ? (
+                            {logado ? (
                                 <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                                     {/* <button
                                         type="button"
@@ -136,7 +136,7 @@ export default function NavBar() {
                                                 <MenuItem>
                                                     {({ active }: { active: boolean }) => (
                                                         <Link
-                                                            href="/perfil"
+                                                            href="/usuario"
                                                             className={classNames(active ? 'bg-gray-800' : '', 'block px-4 py-2 text-sm')}
                                                         >
                                                             <p className='text-theme-text'>Seu Perfil</p>
