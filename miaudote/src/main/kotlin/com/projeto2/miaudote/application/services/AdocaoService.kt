b8367c313ff714b396e06d3f2ce527be7a67e858
Paulina Rehbein <220602eer@gmail.com>
2024-06-03 22:35:06 -0300

    valida confirmacao adocao ao cancelar solicitacao

--- miaudote/src/main/kotlin/com/projeto2/miaudote/application/services/AdocaoService.kt
+++ miaudote/src/main/kotlin/com/projeto2/miaudote/application/services/AdocaoService.kt
@@ -1,8 +1,12 @@
 package com.projeto2.miaudote.application.services
 
+import com.projeto2.miaudote.application.problems.Problem
 import com.projeto2.miaudote.domain.entities.Adocao
+import com.projeto2.miaudote.domain.entities.SolicitacaoAdocao
 import com.projeto2.miaudote.infraestructure.repositories.AdocaoRepository
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import java.net.URI
 import java.util.UUID
 
 @Service
@@ -20,4 +24,16 @@ class AdocaoService(
     fun criar(adocao: Adocao): Adocao{
         return respository.save(adocao)
     }
+}
+fun Adocao?.toProblem(): Result<Adocao> {
+    if (this != null) return Result.success(this)
+    return Result.failure(
+        Problem(
+            title = "Adoção não encontrada",
+            detail = "Não existe adoção para os parametros informados.",
+            type = URI("/obter-adoção"),
+            status = HttpStatus.BAD_REQUEST,
+            extra = null
+        )
+    )
 }
