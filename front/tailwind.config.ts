141e009389bf47cadb7693a6c07ef5f076c1422c
Gabriel Welter <weltergab@gmail.com>
2024-06-06 18:47:33 -0300

    alert solicitar adoção

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -14,6 +14,25 @@ const config: Config = {
       fontFamily: {
         sans: 'Roboto, sans-serif',
       },
+      keyframes: {
+        hide: {
+          from: { opacity: '1' },
+          to: { opacity: '0' },
+        },
+        slideIn: {
+          from: { transform: 'translateX(calc(100% + var(--viewport-padding)))' },
+          to: { transform: 'translateX(0)' },
+        },
+        swipeOut: {
+          from: { transform: 'translateX(var(--radix-toast-swipe-end-x))' },
+          to: { transform: 'translateX(calc(100% + var(--viewport-padding)))' },
+        },
+      },
+      animation: {
+        hide: 'hide 100ms ease-in',
+        slideIn: 'slideIn 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        swipeOut: 'swipeOut 100ms ease-out',
+      },
       colors: {
         theme: {
           'primary': '#7AC7E3',
