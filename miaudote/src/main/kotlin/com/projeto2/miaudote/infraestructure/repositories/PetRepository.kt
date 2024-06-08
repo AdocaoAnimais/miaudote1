338eb890246a7aca62b4fa1db13bc1f2af79713f
Paulina Rehbein <220602eer@gmail.com>
2024-06-08 17:16:30 -0300

    Ajusta obtem pet dos usuarios

--- miaudote/src/main/kotlin/com/projeto2/miaudote/infraestructure/repositories/PetRepository.kt
+++ miaudote/src/main/kotlin/com/projeto2/miaudote/infraestructure/repositories/PetRepository.kt
@@ -5,7 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 
-interface PetRepository : JpaRepository<Pet, Long>{
+interface PetRepository : JpaRepository<Pet, Long> {
+
+    @Query(
+        """
+        select pet from Pet pet 
+        where pet.idUsuario = :idUsuario
+        and pet.id not in (
+            select adocao.petId from Adocao adocao
+        )
+        """
+    )
     fun findByIdUsuario(@Param("idUsuario") idUsuario: Long): List<Pet>?
 
     @Query(
