574a25614dc25708cb682fd87bbdc1dd3038d2b2
“Mari” <mbgroff@stanford.edu>
2024-06-11 15:21:53 -0300

    add imagem aba

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Miaudote',
   description: 'Adoção de animais.', 
+  icons: {
+    icon: '/icon.ico',
+  }
 }
 
 export default function RootLayout({
