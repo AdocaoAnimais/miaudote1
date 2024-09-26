64aa0caeddbf0cae3eeae282fb86304afc2c53fb
Paulina Rehbein <220602eer@gmail.com>
2024-09-26 19:30:49 -0300

    feat: configura h2 para db local

--- miaudote/build.gradle.kts
+++ miaudote/build.gradle.kts
@@ -27,6 +27,7 @@ repositories {
 }
 
 dependencies {
+	implementation("com.h2database:h2")
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-data-rest")
